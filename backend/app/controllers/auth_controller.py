f4e9048598e383dd15213c6dd6163b5fc662947a
DiazFede <diazfedericoucu@gmail.com>
2025-06-23 20:22:30 -0300

    A

--- backend/app/controllers/auth_controller.py
+++ backend/app/controllers/auth_controller.py
@@ -1,8 +1,12 @@
 from app.db import insert_data, get_one
 from flask_jwt_extended import create_access_token
 from datetime import timedelta
+import re
 
 def registrar_ciudadano(data):
+    # Parsear CI para que solo contenga números
+    ci = re.sub(r'\D', '', data.get('CI', ''))
+
     # Verificar si ya existe
     existe = get_one("SELECT 1 FROM Ciudadano WHERE numero_credencial = %s", (data.get('numero_credencial'),))
     if existe:
@@ -14,7 +18,7 @@ def registrar_ciudadano(data):
     """
     params = (
         data.get('numero_credencial'),
-        data.get('CI'),
+        ci,
         data.get('nombre'),
         data.get('apellido'),
         data.get('edad')
@@ -22,9 +26,12 @@ def registrar_ciudadano(data):
     return insert_data(query, params)
 
 def login_ciudadano(data):
+    # Parsear CI para que solo contenga números
+    ci = re.sub(r'\D', '', data.get('CI', ''))
+
     # Validar credenciales
     ciudadano = get_one("SELECT * FROM Ciudadano WHERE numero_credencial = %s AND CI = %s",
-                       (data.get('numero_credencial'), data.get('CI')))
+                        (data.get('numero_credencial'), ci))
     if not ciudadano:
         return None
     
